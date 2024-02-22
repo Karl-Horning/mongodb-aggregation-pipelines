92acf7d52e5036af4ba2af62b4257abdea5c748c
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-22 13:16:45 +0000

    Added getAllPhoneNumbersStartingWith and numUsersWithPhoneNumber aggregations

---
+++ mongo/aggregations/getAllPhoneNumbersStartingWith.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+    {
+        $match: {
+            "company.phone": {
+                $regex: "^\\+1\\s*\\(940\\)",
+            },
+        },
+    },
+];
+
+const client = await MongoClient.connect("mongodb://localhost:27017/");
+const coll = client.db("testData").collection("users");
+const cursor = coll.aggregate(agg);
+const result = await cursor.toArray();
+await client.close();
