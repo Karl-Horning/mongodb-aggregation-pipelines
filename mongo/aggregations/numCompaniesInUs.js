56109c10ce18762f536a07692c5a9ae791890dbe
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-22 15:48:15 +0000

    Added companiesInUs and numCompaniesInUs aggregations

---
+++ mongo/aggregations/numCompaniesInUs.js
@@ -0,0 +1,28 @@
+import { MongoClient } from "mongodb";
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+    {
+        $match: {
+            "company.location.country": "USA",
+        },
+    },
+    {
+        $group: {
+            _id: null,
+            userCount: {
+                $sum: 1,
+            },
+        },
+    },
+];
+
+const client = await MongoClient.connect("mongodb://localhost:27017/");
+const coll = client.db("testData").collection("users");
+const cursor = coll.aggregate(agg);
+const result = await cursor.toArray();
+await client.close();
